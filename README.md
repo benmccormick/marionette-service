7e19cac76a9aae58b9d6fdb10ae9e4b225612ec4
Ben McCormick <ben.mccormick@windsorcircle.com>
2015-04-04 00:35:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Marionette Service
+
+This is a library to add "Service Objects" to Marionette 2.1.x.  Service Objects are an extension of Marionette.Object that
+allows declarative interaction with Backbone.Radio messages. Service Objects can respond to any of Radio's three message types; `Events`, `Commands` and `Requests`.  The syntax is similar to the `events` syntax from Backbone Views, and looks like this:
+
+```js
+radioEvents: {
+  'app start': 'onAppStart',
+  'books finish': 'onBooksFinish',
+},
+
+radioCommands: {
+  'app doFoo': 'executeFoo',
+},
+
+radioRequests: {
+  'resources bar': 'getBar',
+},
+```
+
+where each hash value is in the form `'channel eventName' : 'handler'`.  So
+
+```js
+radioCommands: {
+  'app doFoo': 'executeFoo',
+},
+```
+
+means that the Service will listen for the `doFoo` command on the `app` channel, and run the 'executeFoo' method.  When using Radio Commands and Requests with Objects, the same rules and restrictions that normal Radio use implies also apply here: a single handler can be associated with a command or request, either through manual use of the comply or reply functions, or through the Service API.
+
+
+## Questions
+
+### Wouldn't this be a good fit for Marionette Core?
+
+There is a [PR](https://github.com/marionettejs/backbone.marionette/pull/2431) to include this feature in Marionette v3.0.0.  This library is
+intended as a way to use the feature in 2.1+ apps without having to wait for 3.0.
+
+### Why add a separate Service class instead of shimming the future behavior on Mn.Object?
+
+Marionette 3.0 isn't finalized yet.  APIs may change in the meantime.  I don't want updating to 3.0 to break any existing service code.  If the API does change I will try to track it with this library, but will do so in a semver fashion.  Assuming that the API does remain stable, upgrading to the core code will consist of renaming all `Service` objects to `Object` and removing this dependency.
---
+++ marionette-service.js
@@ -0,0 +1,98 @@
+/**
+ * Marionette Service
+ *
+ * Adds a Service Object to Marionette which allows an Object to
+ * receive Backbone.Radio messages in a declarative fashion.
+ *
+ * @author Ben McCormick
+ *
+ */
+(function (global, factory) {
+    if(typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = factory(require('backbone.marionette'), require('backbone.radio'), require('underscore'));
+    } else if(typeof define === 'function' && define.amd) {
+        define(['backbone.marionette', 'backbone.radio', 'underscore'], factory);
+    } else {
+        factory(global.Marionette, global.Backbone.Radio, global._);
+    }
+})(this, function (Mn, Radio, _) {
+
+    //Proxy Radio message handling to enable declarative interactions with radio channels
+    var radioAPI = {
+        'radioEvents' : {
+            startMethod: 'on',
+            stopMethod: 'off'
+        },
+        'radioCommands' : {
+            startMethod: 'comply',
+            stopMethod: 'stopComplying'
+        },
+        'radioRequests' : {
+            startMethod: 'reply',
+            stopMethod: 'stopReplying'
+        }
+    };
+
+    function proxyRadioHandlers() {
+        unproxyRadioHandlers.apply(this);
+        _.each(radioAPI, function(commands, radioType) {
+            var hash = _.result(this, radioType);
+            if (!hash) {
+                return;
+            }
+            _.each(hash, function(handler, radioMessage) {
+                handler = normalizeHandler.call(this, handler);
+                if (!handler) {
+                  return;
+                }
+                var messageComponents = radioMessage.split(' '),
+                  channel = messageComponents[0],
+                  messageName = messageComponents[1];
+                proxyRadioHandler.call(this,channel, radioType, messageName, handler);
+            }, this);
+        }, this);
+    }
+
+    function proxyRadioHandler(channel, radioType, messageName, handler) {
+        var method = radioAPI[radioType].startMethod;
+        this._radioChannels = this._radioChannels || [];
+        if(!_.contains(this._radioChannels, channel)) {
+            this._radioChannels.push(channel);
+        }
+        Radio[method](channel, messageName, handler, this);
+    }
+
+    function unproxyRadioHandlers() {
+        _.each(this._radioChannels, function(channel) {
+            _.each(radioAPI,function(commands) {
+                Radio[commands.stopMethod](channel, null, null, this);
+            }, this);
+        }, this);
+    }
+
+    function normalizeHandler(handler) {
+        if (!_.isFunction(handler)) {
+            handler = this[handler];
+        }
+        return handler;
+    }
+
+    var Service = Mn.Object.extends({
+
+        constructor: function() {
+            Mn.Object.apply(this);
+            proxyRadioHandlers.apply(this);
+        },
+
+        destroy: function() {
+            Mn.Object.destroy.apply(this);
+            unproxyRadioHandlers.apply(this);
+        }
+
+
+    });
+
+    Mn.Service = Service;
+
+    return Service;
+});
